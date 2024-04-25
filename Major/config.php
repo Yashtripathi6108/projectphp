013fc689cbc8dfa61472321777611e070264865c
Pranjal Tripathi <91819357+Yashtripathi6108@users.noreply.github.com>
2024-04-25 21:24:43 +0530

    project-3

---
+++ Major/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$keyId = 'rzp_test_mgRiTJ5azIAXnm';
+$keySecret = 'z5as3rIWl3gcisZBqT3gTzBo';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+//DATABASE CONNECTION DETAILS
+$host = "localhost";
+$username = "root";
+$password = "Prabal@32415";
+$dbname = "database";
